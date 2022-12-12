b4e7fd5a367c59795091c0791a18c81c45c79a27
Niniatura <neli.marcos@gmail.com>
2022-12-12 13:18:01 -0300

    update de producto

--- manejoDeArchivos.js
+++ manejoDeArchivos.js
@@ -4,12 +4,12 @@ const path = require('path');
 class ProductManager{
     constructor(){
     this.products =[];
-    this.path = path.join('./products.json');
+    this.file = path.join(__dirname, "products.json");
     }
     fileExists(){
         return fs.existsSync(this.file);
     }
-    addProduct(id,
+    addProduct(
         title,
         description,
         price,
@@ -17,7 +17,7 @@ class ProductManager{
         code,
         stock){
         const product = {
-            id: this.products.length + 1,
+            id: Math.floor(Math.random() * 100),
             title: title || '',
             description: description || '',
             price: price || '',
@@ -29,7 +29,7 @@ class ProductManager{
         return new Promise((resolve, reject) => {
             if (this.fileExists()){
                 console.log('El archivo existe')
-                fs.readFile('./products.json', (err, data) => {
+                fs.readFile(this.file, (err, data) => {
                     if(err){
                         return console.log('Error al leer el archivo')
                     }
@@ -44,7 +44,7 @@ class ProductManager{
                 });
             } else {
                 this.products.push(product);
-                fs.writeFileSync('./products.json', JSON.stringify(this.products), 'utf8');
+                fs.writeFileSync(this.file, JSON.stringify(this.products), 'utf8');
                 resolve();
             }
         });
@@ -58,18 +58,19 @@ class ProductManager{
                 this.products = JSON.parse(data);
                 const product = this.products.find((product) => product.id === id);
                 resolve(product);
+                console.log('el producto con id 55 es' + JSON.stringify(product))
             });
         });
     }
     getProducts() {
         return new Promise((resolve, reject) => {
-          fs.readFile('./products.json', "utf8", (err, data) => {
+          fs.readFile(this.file, "utf8", (err, data) => {
             if (err) {
               reject(err);
             }
             this.products = JSON.parse(data);
             resolve(this.products);
-            console.log(data)
+            console.log("el json contiene" + data)
         });
         });
       }
@@ -92,17 +93,32 @@ class ProductManager{
               reject(err);
             }
             this.products = JSON.parse(data);
-            this.products = this.products.filter((product) => product.id !== id);
-            fs.writeFile;
+            let pepe=this.products
+            // // let productToModify = product;
+            // pepe.description=JSON.parse('descripcion modificada');
+            pepe.forEach((product) => {
+                // product = this.products.filter((product) => product.id !== id);
+                // If the bookID is the one we are looking for, set it as null
+                if (product.id === id) {
+                    product.description = 'ahora son verdes';
+                    fs.writeFileSync(this.file, JSON.stringify(data, null, 4), 'utf8');
+                    console.log('el producto modificado es' + JSON.stringify(product))
+                console.log('hola')
+                }
+              });
+              
+            //   console.log(data);
           });
         });
       }
 }
 module.exports=ProductManager;
 const productManager = new ProductManager();
-productManager.addProduct(this.id, 'producto 1', 'description', 180, 'imageUrl', 1234, 45);
-console.log(productManager.getProducts())
-// console.log(productManager.getProductById(3))
-// productManager.updateProduct(this.id, 'producto 1', 'description modificada', 180, 'imageUrl', 1234, 45);
+productManager.addProduct('tomates', 'rojos', 180, 'imageUrl', 1234, 45);
+productManager.addProduct('peras', 'amarillas', 2939, 'imageUrl', 08234, 48);
+console.log(productManager.getProducts());
+console.log(productManager.getProductById((67)));
+console.log(productManager.updateProduct(67))
+// console.log(productManager.updateProduct(39));
 // productManager.deleteProductById(1)
 // console.log(getProductById(1))
